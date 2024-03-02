029aa154f573ac3bedcf8d46b5f8bfcad7a9f2eb
TellMin <dhgirkcls@gmail.com>
2024-03-02 17:53:10 +0900

    solve 309_b

--- abc309/src/bin/b.rs
+++ abc309/src/bin/b.rs
@@ -21,7 +21,23 @@ struct Solver {}
 impl Solver {
     #[fastout]
     fn solve(&mut self) {
-        input! {}
+        input! {
+            n: usize,
+            mut grid: [Chars; n],
+        }
+
+        let mut ans = grid.clone();
+
+        for i in 0..n - 1 {
+            ans[0][i + 1] = grid[0][i];
+            ans[i + 1][n - 1] = grid[i][n - 1];
+            ans[n - 1][n - 1 - i - 1] = grid[n - 1][n - 1 - i];
+            ans[n - 1 - i - 1][0] = grid[n - 1 - i][0];
+        }
+
+        for line in ans {
+            println!("{}", line.iter().join(""));
+        }
     }
 }
 
